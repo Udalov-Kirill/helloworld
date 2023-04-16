4b60b879a1e9c5dde9009e57a7a61647ff5d735a
Udalov-Kirill <oracle-git@mail.ru>
2023-04-16 12:17:27 +0700

    first project

---
+++ HelloWorld_Git/HelloWorld_Git/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HelloWorld_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Oh, hi, Mark!");
+            Console.ReadKey();
+        }
+    }
+}
